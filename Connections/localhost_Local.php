3d016fada6044dbf9b81bc8adfc6dc35cfde887f
arindam-dutta <arindam_duta@yahoo.com>
2015-11-21 23:09:51 +0530

    Connection Change

--- Connections/localhost_Local.php
+++ Connections/localhost_Local.php
@@ -5,10 +5,9 @@ error_reporting(E_ALL ^ E_DEPRECATED);
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_localhost = "us-cdbr-azure-east-a.cloudapp.net";
-$database_localhost = "prescriAvpzXeqTu";
-$username_localhost = "bf7f5c6469ff9b";
-$password_localhost = "c3f02b41";
+$hostname_localhost = "localhost";
+$database_localhost = "registeruser";
+$username_localhost = "root";
+$password_localhost = "";
 $localhost = mysql_connect($hostname_localhost, $username_localhost, $password_localhost) or trigger_error(mysql_error(),E_ERROR | E_PARSE); 
 ?>
-
--- pv/lib/connect.php
+++ pv/lib/connect.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysql_connect("localhost","root","") or die('connection error');
-$db = mysql_select_db("registeruser",$con) or die('database error');
+$con = mysql_connect("us-cdbr-azure-east-a.cloudapp.net","bf7f5c6469ff9b","c3f02b41") or die('connection error');
+$db = mysql_select_db("prescriAvpzXeqTu",$con) or die('database error');
 ?>
